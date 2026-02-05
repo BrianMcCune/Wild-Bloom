1122e0c6f1e9246a0bc13e2b4bfd0344e91d8da3
Brian <brian.mccune1@yahoo.com>
2026-02-05 02:04:21 -0600

    navbar base setup and routing

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
+import styles from './components.module.css'
+
 const Navbar = () => {
     return ( 
-        <div>
-            
-        </div>
+        <nav className={styles.navbar}>
+            <Link href={'/'}>WILD BLOOM</Link>
+            <div className={styles.container}>
+                <Link href={'/shop'}>Shop</Link>
+                <Link href={'/newArrivals'}>New Arrivals</Link>
+                <Link href={'/about'}>About</Link>
+                <Link href={'/contact'}>Contact</Link>
+                <Link href={'cart'}>Cart</Link>
+            </div>
+        </nav>
      );
 }
  
